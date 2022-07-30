46c7c5927ef1fdc924ece023efdc530039019488
PaolaP <paolaperez8@gmail.com>
2022-07-30 12:58:47 -0400

    use describe for organizing

--- resources/mocha/test/sayhello.test.js
+++ resources/mocha/test/sayhello.test.js
@@ -1,16 +1,18 @@
 const sayHello = require('../manual_testing/manual_testing');
 const assert = require('assert');
 
-//describe
+describe('I group tests together, use a file ej. SayHello', () => {
 
-it('describes the things we want to test', () => {
-// testing logic here
-//throw new Error("oops something went wrong")
-});
-
-it('returns "hello there Alice" when given the string "Alice"', () => {
-  const actual = sayHello('Alice');
-  const expected = 'hello there Alice';
-
-  assert.strictEqual(actual, expected);
+  it('describes the things we want to test', () => {
+    // testing logic here
+    //throw new Error("oops something went wrong")
+    });
+    
+    it('returns "hello there Alice" when given the string "Alice"', () => {
+      const actual = sayHello('Alice');
+      const expected = 'hello there Alice';
+    
+      assert.strictEqual(actual, "helloooo");
+    })
 })
+
